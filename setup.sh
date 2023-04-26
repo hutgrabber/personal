3f5241aac41d05b6bace7f85c69b0ec884a9401c
hutgrabber <sparsh@buttstabber-2.mynetworksettings.com>
2023-04-26 11:10:04 -0400

    update

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "(*) Starting Environment Setup for buttstabber !"
-echo "(*) First let's Brew some coffee :-"
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew update
-brew upgrade
