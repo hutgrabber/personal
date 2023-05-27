9681617e49c89ce0d93a761411cdc0733ca7752d
sparsh <sparsh.mehta0598@gmail.com>
2023-05-27 09:13:19 -0400

    updated

--- thm/pickle_rick/tinker.py
+++ thm/pickle_rick/tinker.py
@@ -2,6 +2,12 @@ import requests
 
 url = 'http://10.10.160.253/login.php'
 
+f = open('./demo.txt', 'r')
+output = f.read()
+ls = '/'.split(output)
+print(ls)
 
-
-req = requests.post(url, )
+# requests.post(url, data={
+#     'username':u,
+#     'password':p
+# })
