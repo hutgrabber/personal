4ecb1f66d8525bd63890c58c288eaa4de76085a9
hutgrabber <sparsh.mehta0598@gmail.com>
2023-06-02 00:27:11 -0400

    updated

--- thm/mr_robot/cleanup.py
+++ thm/mr_robot/cleanup.py
@@ -1,4 +1,13 @@
+import re
+file_path = '/Users/sparsh/Projects/secret-code-stash/personal-git/thm/mr_robot/'
+
 words = open(file_path + 'words.txt', 'r').readlines()
 formatted = []
 for i in words:
     formatted.append(re.sub('\n', '', i))
+
+remove_dups = list(dict.fromkeys(formatted))
+print(remove_dups)
+
+with open(file_path + "new.txt", "w") as output:
+    output.write(str(remove_dups))
